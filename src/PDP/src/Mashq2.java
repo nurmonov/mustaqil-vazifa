28b9d9820e4e33ffb3de68f037b2d64d952a5b12
nurmonov <nurmonoff@gmail.com>
2024-09-25 17:36:27 +0500

    Pdp

---
+++ src/PDP/src/Mashq2.java
@@ -0,0 +1,14 @@
+package lesson9;
+
+import java.util.ArrayList;
+
+public class Mashq2 {
+
+    public static void main(String[] args) {
+        ArrayList student = new ArrayList<Student>();
+        student.add(new Student("aliw","futbol",21));
+        System.out.println(student);
+
+    }
+
+}
